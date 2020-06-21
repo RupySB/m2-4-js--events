c62bf4a68cfcf749727b9e3157610b01f391515d
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-21 17:47:16 -0400

    Q1.1 ReDone JS

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -3,17 +3,19 @@
 
 let body = document.querySelector("body");
 let result = document.getElementById("result");
-let clickedFast=false;
+let clickedFast = false;
+
+// The 'click' function
 function clickEvent() {
   clickedFast = true;
   result.innerText = "You Win!";
   body.removeEventListener("click", clickEvent);
 }
-setTimeoutn(function (){
-    if (!clickedFast) {
-        result.innerText="You Lose!";
-        body.removeEventListener("click", clickEvent);
-    }, 
-},1500);
+setTimeout(function () {
+  if (!clickedFast) {
+    result.innerText = "You Lose!";
+    body.removeEventListener("click", clickEvent);
+  }
+}, 1500);
 body.addEventListener("click", clickEvent);
 //console.log("exercise 1.1");
