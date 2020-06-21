876be1add3d4031502aebed4518b0eb960309827
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-21 14:55:26 -0400

    Q1 JS

--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,11 @@
 // Exercise 1.0
 // ------------
-console.log('exercise-1');
+
+const body = document.querySelector("body");
+const result = document.getElementById("clickreveal");
+function clickEvent() {
+  result.innerText = "You've won a prize!";
+  body.removeEventListener("click", clickEvent);
+}
+body.addEventListener("click", clickEvent);
+console.log("exercise-1");
