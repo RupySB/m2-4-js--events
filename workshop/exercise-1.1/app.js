db3b6ed23a1aaea6576943bb070a6220bd057ce8
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-21 17:30:35 -0400

    Q1.1 JS

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,19 @@
 // Exercise 1.1
 // ------------
-console.log('exercise 1.1');
+
+let body = document.querySelector("body");
+let result = document.getElementById("result");
+let clickedFast=false;
+function clickEvent() {
+  clickedFast = true;
+  result.innerText = "You Win!";
+  body.removeEventListener("click", clickEvent);
+}
+setTimeoutn(function (){
+    if (!clickedFast) {
+        result.innerText="You Lose!";
+        body.removeEventListener("click", clickEvent);
+    }, 
+},1500);
+body.addEventListener("click", clickEvent);
+//console.log("exercise 1.1");
