573faa402ab435f4ae602922a7eec950f9423708
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-21 22:19:42 -0400

    Q1.2 JS

--- workshop/exercise-1.2/app.js
+++ workshop/exercise-1.2/app.js
@@ -1,3 +1,24 @@
 // Exercise 1.2
 // ------------
-console.log('exercise 1.2');
+
+let body = document.querySelector("body");
+let result = document.getElementById("result");
+let youWin = false;
+let time = Math.ceil(Math.random() * 4);
+document.getElementById("time").innerText = time;
+
+function clickEvent() {
+  youWin = true;
+  result.innerText = "You Win!";
+  body.removeEventListener("click", clickEvent);
+}
+setTimeout(function () {
+  if (!youWin) {
+    result.innerText = "You Lose";
+    body.removeEventListener("click", clickEvent);
+  }
+}, time * 1500);
+console.log(time);
+body.addEventListener("click", clickEvent);
+
+//how do I change it so I can have a range for the time?
